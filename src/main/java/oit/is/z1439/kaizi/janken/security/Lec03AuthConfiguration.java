0c89458c31a27277a521fd9d7d0852f0ef89f3fd
e1b18075 <e1b18075@oit.ac.jp>
2020-10-23 18:15:19 +0900

    セキュリティの認証処理を実装する

--- src/main/java/oit/is/z1439/kaizi/janken/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z1439/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -1,24 +1,59 @@
 package oit.is.z1439.kaizi.janken.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Sample3AuthConfiguration
+ */
 @Configuration
 @EnableWebSecurity
 public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
 
+  /**
+   * 誰がログインできるか(認証処理)
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    // $ sshrun htpasswd -nbBC 10 user1 pAssw0rd
+    auth.inMemoryAuthentication().withUser("user1")
+        .password("$2y$10$3fXLs5lUlNUTXqa4kcnuguHrI2m2LwdPc5WtXvG4aPx.R9uyqtvuG").roles("USER");
+    auth.inMemoryAuthentication().withUser("user2")
+        .password("$2y$10$k7oaq61hj344mR.vDWUe6eXGbqQiexK0vJtAJTrqUXX4EFGizYORy").roles("USER");
+
+    // 開発中は↓の書き方でも良いが，平文でパスワードが保存される
+    // auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("janken")).roles("USER");
+    // auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder().encode("password")).roles("USER");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * 認証されたユーザがどこにアクセスできるか（認可処理）
+   */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
     // Spring Securityのフォームを利用してログインを行う
     http.formLogin();
 
+    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
+    // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
+    // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
+    // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
     http.authorizeRequests().antMatchers("/lec02/**").authenticated();
 
-    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8080/ に戻る
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
     http.logout().logoutSuccessUrl("/");
   }
-
 }
