5db1ac0cbdcc7b3abe7629dca1578691b494775b
e1b18075 <e1b18075@oit.ac.jp>
2020-10-23 15:40:56 +0900

    じゃんけんの手を選んでCPUと対戦する処理を実装する

--- src/main/java/oit/is/z1439/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1439/kaizi/janken/controller/Lec02Controller.java
@@ -7,11 +7,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z1439.kaizi.janken.model.Janken;
+
 @Controller
 public class Lec02Controller {
 
   @GetMapping("/lec02")
   public String lec02() {
+    return "lec02.html";
+  }
+
+  @GetMapping("/lec02_geam")
+  public String lec02_geam(@RequestParam Integer jankenhand, ModelMap model) {
+
+    Janken hand = new Janken(jankenhand);
+    model.addAttribute("my_hand", hand.playerhand());
+    model.addAttribute("your_hand", hand.cpuhand());
+    model.addAttribute("Result", hand.score());
     return "lec02.html";
   }
 
