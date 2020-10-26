9cbdc88158d1a158c8125e78666f78b330c94839
e1b18075 <e1b18075@oit.ac.jp>
2020-10-26 19:24:40 +0900

    ユーザ情報をSELECTで取得して表示する

---
+++ src/main/java/oit/is/z1439/kaizi/janken/model/User.java
@@ -0,0 +1,22 @@
+package oit.is.z1439.kaizi.janken.model;
+
+public class User {
+  int id;
+  String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setname(String name) {
+    this.name = name;
+  }
+
+  public int getid() {
+    return id;
+  }
+
+  public void setid(int id) {
+    this.id = id;
+  }
+}
