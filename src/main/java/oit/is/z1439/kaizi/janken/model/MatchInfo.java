6cd17acb1519d9cd90b8996a6497a7e3c5e61001
e1b18075 <e1b18075@oit.ac.jp>
2020-11-09 23:18:23 +0900

    DBの初期化処理を実装する

--- src/main/java/oit/is/z1439/kaizi/janken/model/MatchInfo.java
+++ src/main/java/oit/is/z1439/kaizi/janken/model/MatchInfo.java
@@ -1,12 +1,10 @@
 package oit.is.z1439.kaizi.janken.model;
 
-public class Match {
+public class MatchInfo {
 
   int id;
   int user_1;
   int user_2;
-  String user_1_hand;
-  String user_2_hand;
   boolean is_active;
 
   // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
@@ -34,22 +32,6 @@ public class Match {
 
   public void setUser_2(int user_2) {
     this.user_2 = user_2;
-  }
-
-  public String getUser_1_hand() {
-    return this.user_1_hand;
-  }
-
-  public void setUser_1_hand(String user_1_hand) {
-    this.user_1_hand = user_1_hand;
-  }
-
-  public String getUser_2_hand() {
-    return this.user_2_hand;
-  }
-
-  public void setUser_2_hand(String user_2_hand) {
-    this.user_2_hand = user_2_hand;
   }
 
   public boolean getis_active() {
